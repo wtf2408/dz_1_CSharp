8470eb3d5065723db555b97fbe8594bb0d5124a4
Name2408 <bogdanevseyv@yandex.ru>
2022-10-18 18:12:07 +0300

    написал решение 2 задания

---
+++ Задача2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите два числа:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA>numberB)
+{
+    Console.Write(numberA);
+    Console.Write(" = max");
+}
+if (numberB>numberA)
+{
+    Console.Write(numberB);
+    Console.Write(" = max");
+}

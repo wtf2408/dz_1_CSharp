ade50a7797d6b73189e019417f8fc52e25a8042c
Name2408 <bogdanevseyv@yandex.ru>
2022-10-18 19:13:54 +0300

    Передалал 2 номер

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -3,13 +3,20 @@ Console.Write("Введите два числа:");
 int numberA = Convert.ToInt32(Console.ReadLine());
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-if (numberA>numberB)
-{
-    Console.Write(numberA);
-    Console.Write(" = max");
-}
 if (numberB>numberA)
 {
     Console.Write(numberB);
     Console.Write(" = max");
 }
+
+if (numberA == numberB)
+{
+    Console.Write("наибольшего  числа нет");
+    
+}
+if (numberA>numberB)
+{
+    Console.Write(numberB);
+     Console.Write(" = max");
+}
+

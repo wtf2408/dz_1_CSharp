a7022ecb8f9639a31f9a2935b5337aa9270ce929
Name2408 <bogdanevseyv@yandex.ru>
2022-10-18 19:22:50 +0300

    Решил 6 номер

---
+++ Задача6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите числo:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if( numberA %2 == 0)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}

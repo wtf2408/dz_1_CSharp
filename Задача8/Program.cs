81967ec53468ab5bd29b56cf716c55b040823ba8
Name2408 <bogdanevseyv@yandex.ru>
2022-10-18 19:57:40 +0300

    Решил 8 номер

---
+++ Задача8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int count=0;
+int i = 1;
+
+while (count<n)
+{
+    if(i %2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(",");
+    }
+    i+=1;
+    count+=1;
+}

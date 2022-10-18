58319ab9eed7d129400bc119d2c74aa7a29f18a4
Name2408 <bogdanevseyv@yandex.ru>
2022-10-18 18:33:48 +0300

    Решение 4 задания

---
+++ Задача4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите три числа:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+
+if (numberB>max)
+{
+    max = numberB;
+}
+if (numberC>max)
+{
+    max = numberC;
+}
+Console.Write(max); 
+Console.Write(" = max");
